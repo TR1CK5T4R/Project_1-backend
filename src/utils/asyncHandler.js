b4bcaa7e65241c75813ddf289a8cda70f9290203
Tushar Singla <singlatushar@Tushars-MacBook-Air.local>
2026-01-25 15:46:59 +0530

    adding utils and error handlers

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (reqHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next)).catch((error) => next(error));
+    }
+}
+
+export default asyncHandler;
+
+// const asyncHandler = () => { }
+// const asyncHandler = (func) => () => { }
+// const asyncHandler = (fn) => async() => { }
+
+// try-catch:
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     }
+//     catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message || "Internal Server Error"
+//         });
+//     }
+
+// }
